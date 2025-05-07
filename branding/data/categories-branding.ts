b214e62b7eb62a69745636920a89b3972c46cbe8
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-07 21:47:10 +0200

    better

--- branding/data/categories-branding.ts
+++ branding/data/categories-branding.ts
@@ -1,7 +1,114 @@
-import { getNeode } from '@db/neo4j'
+import { getDriver, getNeode } from '@db/neo4j'
+import { mutate, query } from '@context/database'
 
 const neode = getNeode()
+const driver = getDriver()
 
+const categories = [
+  {
+    id: 'corona',
+    slug: 'corona',
+    name: 'corona',
+    icon: '/img/categories/corona.svg',
+  },
+  {
+    id: 'digital_euro',
+    slug: 'digital_euro',
+    name: 'digital_euro',
+    icon: '/img/categories/digital_euro.svg',
+  },
+  {
+    id: 'who',
+    slug: 'who',
+    name: 'who',
+    icon: '/img/categories/who.svg',
+  },
+  {
+    id: 'monetary_system',
+    slug: 'monetary_system',
+    name: 'monetary_system',
+    icon: '/img/categories/monetary_system.svg',
+  },
+  {
+    id: 'basic_rights',
+    slug: 'basic_rights',
+    name: 'basic_rights',
+    icon: '/img/categories/basic_rights.svg',
+  },
+  {
+    id: 'justice',
+    slug: 'justice',
+    name: 'justice',
+    icon: '/img/categories/justice.svg',
+  },
+  {
+    id: 'demonstrations',
+    slug: 'demonstrations',
+    name: 'demonstrations',
+    icon: '/img/categories/demonstrations.svg',
+  },
+  {
+    id: 'child_protection',
+    slug: 'child_protection',
+    name: 'child_protection',
+    icon: '/img/categories/child_protection.svg',
+  },
+  {
+    id: 'transhumanism',
+    slug: 'transhumanism',
+    name: 'transhumanism',
+    icon: '/img/categories/transhumanism.svg',
+  },
+  {
+    id: 'surveillance',
+    slug: 'surveillance',
+    name: 'surveillance',
+    icon: '/img/categories/surveillance.svg',
+  },
+  {
+    id: 'education',
+    slug: 'education',
+    name: 'education',
+    icon: '/img/categories/education.svg',
+  },
+  {
+    id: 'media',
+    slug: 'media',
+    name: 'media',
+    icon: '/img/categories/media.svg',
+  },
+  {
+    id: 'ngos',
+    slug: 'ngos',
+    name: 'ngos',
+    icon: '/img/categories/ngos.svg',
+  },
+]
+
+const removeUndefinedCategories = async () => {
+  const categoryIds = categories.map((c) => c.id)
+  const idPromise = await mutate(driver)({
+    query: `
+      MATCH (category:Category)
+      DETACH DELETE
+        WHERE NOT category.id IN $categoryIds
+      WITH category
+      RETURN category.id AS id
+    `,
+    variables: {
+      categoryIds,
+    },
+  })
+  const ids = idPromise.records.map((r) => r.get('id'))
+  console.log(ids)
+  return
+}
+
+export default async () => {
+  await removeUndefinedCategories()
+}
+
+/*
 export const categories = async () => {
   return [
     await neode.create('Category', {
@@ -84,3 +191,4 @@ export const categories = async () => {
     }),
   ]
 }
+*/
