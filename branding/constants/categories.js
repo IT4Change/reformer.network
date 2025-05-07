2997772307981fecb47115163ae7f2e0d4afa2c4
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-08 01:02:04 +0200

    new categories as constant for seeding

---
+++ branding/constants/categories.js
@@ -0,0 +1,84 @@
+// this file is duplicated in `backend/src/constants/metadata` and `webapp/constants/metadata.js`
+export const CATEGORIES_MIN = 1
+export const CATEGORIES_MAX = 3
+
+export const categories = [
+  {
+    id: 'corona',
+    slug: 'corona',
+    name: 'corona',
+    icon: '/img/categories/corona.svg',
+  },
+  {
+    id: 'digital_euro',
+    slug: 'digital_euro',
+    name: 'digital_euro',
+    icon: '/img/categories/digital_euro.svg',
+  },
+  {
+    id: 'who',
+    slug: 'who',
+    name: 'who',
+    icon: '/img/categories/who.svg',
+  },
+  {
+    id: 'monetary_system',
+    slug: 'monetary_system',
+    name: 'monetary_system',
+    icon: '/img/categories/monetary_system.svg',
+  },
+  {
+    id: 'basic_rights',
+    slug: 'basic_rights',
+    name: 'basic_rights',
+    icon: '/img/categories/basic_rights.svg',
+  },
+  {
+    id: 'justice',
+    slug: 'justice',
+    name: 'justice',
+    icon: '/img/categories/justice.svg',
+  },
+  {
+    id: 'demonstrations',
+    slug: 'demonstrations',
+    name: 'demonstrations',
+    icon: '/img/categories/demonstrations.svg',
+  },
+  {
+    id: 'child_protection',
+    slug: 'child_protection',
+    name: 'child_protection',
+    icon: '/img/categories/child_protection.svg',
+  },
+  {
+    id: 'transhumanism',
+    slug: 'transhumanism',
+    name: 'transhumanism',
+    icon: '/img/categories/transhumanism.svg',
+  },
+  {
+    id: 'surveillance',
+    slug: 'surveillance',
+    name: 'surveillance',
+    icon: '/img/categories/surveillance.svg',
+  },
+  {
+    id: 'education',
+    slug: 'education',
+    name: 'education',
+    icon: '/img/categories/education.svg',
+  },
+  {
+    id: 'media',
+    slug: 'media',
+    name: 'media',
+    icon: '/img/categories/media.svg',
+  },
+  {
+    id: 'ngos',
+    slug: 'ngos',
+    name: 'ngos',
+    icon: '/img/categories/ngos.svg',
+  },
+]
