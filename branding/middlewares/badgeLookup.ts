0a77a35d85bb600dae88163b80d192ca8dbe9756
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-07-06 11:28:53 +0200

    fix user type

--- branding/middlewares/badgeLookup.ts
+++ branding/middlewares/badgeLookup.ts
@@ -30,7 +30,7 @@ const assignBadges = async (email: string, context: Context) => {
   }
 
   // We need the user for the resolver call. We attribute the badge as user: 0
-  context.user = { id: 'badge-lookup' }
+  context.user = { id: 'badge-lookup' } as any
 
   const normalizedEmail = normalizeEmail(email)
   const hash = crypto.createHash('sha256').update(normalizedEmail).digest('hex')
